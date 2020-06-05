311920340354f6cb059d96586f59457460650a0c
brinnaebent <brinnae.bent@duke.edu>
2020-06-05 16:29:59 -0400

    Added v0.1 tarzip

--- setup.py
+++ setup.py
@@ -1,25 +1,27 @@
-from setuptools import setup
-
-def readme():
-    with open('README.rst') as f:
-        return f.read()
-
-from os import path
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
-    ld = f.read()
-    
+from distutils.core import setup
 
 setup(name='wearablevar',
+      packages = ['wearablevar'],
       version='0.1',
       description='Variability Metrics from Longitudinal Wearable Sensors',
-      long_description= ld,
-      long_description_content_type= 'text/markdown',
       url='https://github.com/brinnaebent/wearablevar',
+      download_url = 'https://github.com/brinnaebent/wearablevar/archive/0.1.tar.gz'
       author='Brinnae Bent',
       author_email='runsdata@gmail.com',
+      keywords = ['wearables', 'features', 'feature engineering'],
       license='MIT',
-      packages=['wearablevar'],
+
       install_requires=['pandas','numpy','datetime',
-                        ],
-      zip_safe=False)
+            ],
+
+      classifiers=[
+        'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        'Intended Audience :: Developers',      
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',   
+        'Programming Language :: Python :: 3',      
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        ],
+)
